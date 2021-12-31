6493a61a348f2471a8d010f7e0c12ee8dc9bb6a2
az1dii <azheng1092@gmail.com>
2021-12-30 23:41:43 -0500

    update

--- src/pages/AllTransaction.js
+++ src/pages/AllTransaction.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Post from "../components/transaction";
+import Transaction from "../components/transaction";
 
 const AllTransactions = (props) => {
-  // For each transaction in the array render a Post component
-  return props.transactions.map((transaction) => <Post transaction={transaction} key={transaction.id} />);
+  // For each transaction in the array render a Transaction component
+  return props.transactions.map((transaction) => <Transaction transaction={transaction} key={transaction.id} />);
 };
 
 export default AllTransactions;
